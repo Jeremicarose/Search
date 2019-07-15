9037bdef20cace39f37022cd8ebefd1270a7dfc7
Jeremic <arosejeremic@gmail.com>
2019-07-15 13:17:29 +0300

    change color

--- src/app/user-det.directive.ts
+++ src/app/user-det.directive.ts
@@ -7,13 +7,6 @@ export class UserDetDirective {
 
   constructor(private el: ElementRef) { }
 
-  @HostListener('mouseenter') onMouseEnter() {
-    this.hover('#C46D5E');
-  }
-
-  @HostListener('mouseleave') onMouseLeave() {
-    this.hover('#DAA588');
-  }
 
   private hover(color: string) {
     this.el.nativeElement.style.borderColor = color;
